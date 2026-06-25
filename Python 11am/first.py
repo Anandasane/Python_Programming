205a58c58bfc2e77c9abcc88825db0a741b1729f
Anandasane <anandasane4@gmail.com>
2026-06-25 12:20:10 +0530

    created some new changes

--- Python 11am/first.py
+++ Python 11am/first.py
@@ -1,11 +1,15 @@
 print("you have been selected you have a offer of 12lpa")
-
+#variables declaration 
 a = 10
 b = 30
 c = a + b
 
 print(c)
 
+# multiple variable declaration                 
+a= b= c = 13
+# multiple variable declaration using specific values
+x,y,z = 12,34,22
 def function1():
     print("this is function 1")
     return {
