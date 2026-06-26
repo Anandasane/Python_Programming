436886b72318dcdcc5422cdd78097fea7bb2e5ed
Anandasane <anandasane4@gmail.com>
2026-06-26 11:55:16 +0530

    added a new file

--- Python 11am/first.py
+++ Python 11am/first.py
@@ -10,11 +10,52 @@ print(c)
 a= b= c = 13
 # multiple variable declaration using specific values
 x,y,z = 12,34,22
-def function1():
-    print("this is function 1")
-    return {
-        "a": 10,
-        "b": 20
-    }
 
-function1()
+
+# def function1():
+#     print("this is function 1")
+#     return {
+#         "a": 10,
+#         "b": 20
+#     }
+
+# function1()
+
+# printing type and id for the varaible
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+print(id(a))
+print(id(b))
+print(id(c))
+
+x=20
+print(id(x))
+
+y=44
+print(id(y))
+
+# declarating and printing complex number
+z = 4+5j
+print(z)
+print(type(z))
+
+print(z.real)
+print(z.imag)
+
+t = complex(24,53)
+print(t)
+print(type(t))
+print(t.real)
+print(t.imag)
+
+# declaring String values
+a = "a"
+print(a)
+print(type(a))
+b = 'i am learning python'
+print(b)
+print(type(b))
