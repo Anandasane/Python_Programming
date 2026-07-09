e6ea188a7462f4e93d39b083c74a26f1b26f3ea3
Anandasane <anandasane4@gmail.com>
2026-07-09 12:03:37 +0530

    added new lines of code

--- Python 11am/for_loop.py
+++ Python 11am/for_loop.py
@@ -14,3 +14,28 @@ for i in range(1,101):
     if(i%2==0 and i%5==0 and i%10==0):
         print(i)
 
+
+n=int(input("Enter a number: "))
+c=0
+for i in range(1,n+1):
+    if(i%2==0):
+        print(i)
+        c+=1
+
+print("count is: ",c)
+
+n= int(input("Enter a number: "))
+sum =0
+for i in range(1,n+1):
+    sum = sum+i
+
+print(sum)
+
+s=input("Enter a string: ")
+v=0
+ 
+for i in range(0,len(s)):
+    if(s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u' or s[i]=='A'  or s[i]=='E' or s[i]=='I' or s[i]=='O' or s[i]=='U'):
+        v+=1
+
+print(v)
--- Python 11am/tempCodeRunnerFile.py
+++ Python 11am/tempCodeRunnerFile.py
@@ -1,3 +1,8 @@
-for i in range(1,101):
-    if(i%2==0 and i%5==0 and i%10==0):
-        print(i)
+s=input("Enter a string: ")
+v=0
+ 
+for i in range(0,len(s)):
+    if(s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u' or s[i]=='A'  or s[i]=='E' or s[i]=='I' or s[i]=='O' or s[i]=='U'):
+        v+=1
+
+print(v)
