df1b28812c83cd8567497de27ef9ce2f5af6dc69
Anandasane <anandasane4@gmail.com>
2026-08-18 11:40:48 +0530

    added new lines

--- Python 11am/oops in python/class1.py
+++ Python 11am/oops in python/class1.py
@@ -1,4 +1,4 @@
-print('-------------------ooops classes ------------------------------------------------------------------------')
+print('-------------------------------------------------oops classes ----------------------------------------------------------------')
 
 class Student:    
     name= 'chetan'
@@ -14,8 +14,8 @@ class Student:
        print("Student Roll no is : ",Student.rollno)
 
     def Entername(name,self):
-       s=input()
-       print('changed student name is : ', s)
+       #s=input()
+       print('changed student name is : ',name)
 
 
 s=Student()
@@ -25,3 +25,34 @@ s.Entername('z')
 
 print(s.name)
 print(s.age)
+
+print('================================Bike class=========================================================')
+class bike:
+   name = "hayabusa"
+   color='Red'
+   price=999999
+
+   def top_speed(self):
+      print('Top speed is : 250')
+
+   def show_details(self):
+      print('Name of the bike: ',self.name,'\nPrice is :',self.price,'\ncolor: ',self.color)
+
+   # default Constructor
+   def __init__(self):
+      pass
+   # No Argument constructor 
+   def __init__(self):
+      print('constructor is called  when object is created ')
+      self.price=12000
+      self.color='Yellow'
+      self.name='jawa'
+
+
+b=bike()
+c=bike()
+b.top_speed()
+b.show_details()
+print(b.color)
+
+
